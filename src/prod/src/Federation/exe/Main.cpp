cafbed592cd29815562bfb28114d2ff86acdc395
Alex Wun <alexwun@microsoft.com>
2018-10-09 17:30:29 -0700

    Update FederationRuntime.exe to accept arguments. Simple two-node local cluster connects.

--- src/prod/src/Federation/exe/Main.cpp
+++ src/prod/src/Federation/exe/Main.cpp
@@ -17,41 +17,84 @@ using namespace Transport;
 
 StringLiteral const TraceComponent("Main");
 
+using lchar_t = wchar_t;
+
+wstring ToWString(string const & in)
+{
+    static wstring_convert<codecvt_utf8_utf16<lchar_t>, lchar_t> cvt;
+
+    wstring result;
+    for (auto const & ch : cvt.from_bytes(in))
+    {
+        result.push_back(ch);
+    }
+
+    return result;
+}
+
 string ToString(wstring const & in)
 {
-    static wstring_convert<codecvt_utf8_utf16<wchar_t>, wchar_t> cvt;
+    static wstring_convert<codecvt_utf8_utf16<lchar_t>, lchar_t> cvt;
     return cvt.to_bytes(in.c_str());
 }
 
+const size_t MaxArgLen = 128;
+
 int main(int argc, char* argv[])
 {
-    // TODO: read from arguments
-    wstring nodeIdString = L"0";
-    wstring nodeAddress = L"localhost:8080";
-    wstring leaseAgentAddress = L"localhost:8081";
-    wstring workingDirectory = L"./";
-
-    printf("Running at %s. Press any key to continue... \n", ToString(nodeAddress).c_str());
-
-    getchar();
-
-    Trace.WriteInfo(TraceComponent, "Running main...");
-
-    NodeId nodeId(LargeInteger(0, 0));
-    if (!NodeId::TryParse(nodeIdString, nodeId))
-    {
-        printf("Failed to parse '%s' as NodeId \n", ToString(nodeIdString).c_str());
+    if (argc < 3)
+    {   
+        printf("usage: %s -n <node ID> -p [port] -h [hostname/ip (default='localhost')] \n", argv[0]);
+        printf("example: %s -n 10 -p 19000\n", argv[0]);
         return 1;
     }
+
+    string nodeIdString = "0";
+    string hostname = "localhost";
+    unsigned int port = 19000;
+    wstring workingDirectory = L"./";
+
+    for (auto ix=0; ix<argc; ++ix)
+    {
+        if (ix+1 < argc)
+        {
+            if (strncmp(argv[ix], "-n", MaxArgLen) == 0)
+            {
+                nodeIdString = argv[++ix];
+            }   
+            else if (strncmp(argv[ix], "-p", MaxArgLen) == 0)
+            {
+                port = atoi(argv[++ix]);
+            }
+            else if (strncmp(argv[ix], "-h", MaxArgLen) == 0)
+            {
+                hostname = argv[++ix];
+            }
+        }
+    }
+
+    NodeId nodeId;
+    if (!NodeId::TryParse(ToWString(nodeIdString), nodeId))
+    {
+        printf("Failed to parse '%s' as NodeId \n", nodeIdString.c_str());
+        return 1;
+    }
+
+    string nodeAddress = hostname + ":" + to_string(port);
+    string leaseAddress = hostname + ":" + to_string(port + 1);
+
+    printf("Running node %s at %s...\n",
+        nodeIdString.c_str(), 
+        nodeAddress.c_str());
 
     ComponentRoot mockRoot;
     auto federation = std::make_shared<Federation::FederationSubsystem>(
         NodeConfig(
             nodeId, 
-            nodeAddress, 
-            leaseAgentAddress, 
+            ToWString(nodeAddress), 
+            ToWString(leaseAddress), 
             workingDirectory),
-        FabricCodeVersion(),
+        FabricCodeVersion(1, 0, 960, 0), // minimum acceptable version at Federation layer
         Uri(),
         SecuritySettings(),
         mockRoot);
